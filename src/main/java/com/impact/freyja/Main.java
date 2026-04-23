4bce7a2eeada93fddb86e39cf0878945d258afd6
Knut Forkalsrud <knut@impact.com>
2026-04-23 11:10:29 +0200

    application

---
+++ src/main/java/com/impact/freyja/Main.java
@@ -0,0 +1,13 @@
+package com.impact.freyja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
